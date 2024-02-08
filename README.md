5aae15d0037dc99e7ecb6893f99a1cd397675d66
SUJAY N GOWDA <1sujay1>
2024-02-08 20:56:40 +0530

    all auto deployment done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# CI-CD-JENKINS" 
+"# CI-CD-JENKINS"
+
+CRON JOB EVERY MINUTE
+"asterisk/1 asterisk asterisk asterisk asterisk"
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Congratulations!!!</h1><p>Server is Running</p>`);
+  res.send(
+    `<h1>Congratulations!!!</h1><p>Server is Running with Auto Deployment</p>`
+  );
 });
 app.listen(4000, () => {
   console.log("Server Listening to PORT 4000...");
